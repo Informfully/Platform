e2024a629f5df6121413ef5fb06c9f25ded6e441
Lucien Heitz <lucienheitz@msn.com>
2024-05-24 18:16:23 +0200

    Update .snippets.sh

--- backend/.snippets.sh
+++ backend/.snippets.sh
@@ -9,14 +9,14 @@ set -e;
 # -----------------------------------------------------
 
 # the IP address of the server where the build will be deployed to
-DEPLOYMENT_HOST="urza.ifi.uzh.ch";
+DEPLOYMENT_HOST="[SERVER]";
 # user used to deploy the application
-DEPLOYMENT_USER="appuser";
+DEPLOYMENT_USER="[USER]";
 # directory where the bundle will be uploaded to
 UPLOADS_DIR="/home/${DEPLOYMENT_USER}/uploads/";
 # directory where the app is running
-APP_DIR="/var/www/newsapp/";
+APP_DIR="/var/www/[DIRECTORY]/";
 # temporary directory for deployment
-APP_TMP_DIR="/var/www/newsapp/tmp/";
+APP_TMP_DIR="/var/www/[DIRECTORY]/tmp/";
 # directory to store old builds
-BUNDLES_DIR="/var/www/newsapp/builds/";
+BUNDLES_DIR="/var/www/[DIRECTORY]/builds/";
